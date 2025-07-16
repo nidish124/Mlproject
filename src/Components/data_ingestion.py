245113e2eb91e631df34c99f2e8baf3e62bce836
nidish124 <nidish124@gmail.com>
2025-07-16 15:58:46 +0100

    data ingestion

---
+++ src/Components/data_ingestion.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from src.Exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+import os 
+import sys
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path : str = os.path.join("artifacts", "train.csv")
+    test_data_path : str = os.path.join("artifacts", "test.csv")
+    raw_data_path : str = os.path.join("artifacts", "data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Initiating data ingestion...')
+        try:
+            df = pd.read_csv('data\stud.csv')
+            logging.info('csv read is completed')
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+
+            logging.info("train test split has been started")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=43)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+            logging.info('Data ingestion completed successfully.')
+
+            return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+        
+        except Exception as e:
+            logging.info(f'Error occurred during data ingestion: {e}')
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    dataingestion = DataIngestion()
+    dataingestion.initiate_data_ingestion()
